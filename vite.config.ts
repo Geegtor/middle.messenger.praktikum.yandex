aa41c2ecb853a2510ed3a5e58ccf87d4454d2f31
callmedim <8431947@gmail.com>
2023-09-12 03:38:11 +0300

    initiall commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        outDir: "./build"
+    },
+    css: {
+        modules: {
+            localsConvention: "camelCase",
+            generateScopedName: "[name]_[local]__[hash:base64:4]"
+        }
+    }
+})
