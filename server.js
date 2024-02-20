1eb7b84d9e264c611de3dc3f80c0e839df6b3670
callmedim <8431947@gmail.com>
2024-02-20 07:00:25 +0300

    routes for server

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const port = 3000;
 
 app.use(express.static('./build/'));
 
+app.get("/", function (req, res) {
+  res.send("hello sign-in");
+});
+
+app.get("/sign-up", function (req, res) {
+  res.send("hello sign-up");
+});
+
+app.get("/settings", function (req, res) {
+  res.send("hello settings");
+});
+
+
+app.get("/messanger", function (req, res) {
+  res.send("hello messanger");
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
