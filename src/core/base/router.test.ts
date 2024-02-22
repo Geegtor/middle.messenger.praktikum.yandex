0c1d7f34878f6a77854cfbd5fb34fd5ebbe53812
callmedim <8431947@gmail.com>
2024-02-22 17:51:02 +0300

    base components router and block test impl

---
+++ src/core/base/router.test.ts
@@ -0,0 +1,63 @@
+import sinon from "sinon";
+import { expect } from "chai";
+import { Route } from "../route";
+
+import { RouterClass } from "../router";
+import Block, { Props } from "./block";
+
+describe("A Mocha init test", () => {
+    it("Simple green test:", function (done) {
+        setImmediate(done);
+    });
+})
+
+describe("Router tests", () => {
+    let TestRouter: RouterClass;
+    let TestComponent: typeof Block;
+
+    before(() => {
+        class TestClass extends Block {
+            constructor(props: Props){
+                super();
+            }
+            protected render() {
+                return "<div>data</div>"
+            } 
+        }
+        TestComponent = new TestClass({}) as unknown as typeof Block;
+        TestRouter = new RouterClass("#app");
+    })
+
+    it("method 'getRoute' returns Route instace", () => {
+        TestRouter.use("/path", TestComponent);
+        const route = TestRouter.getRoute("/path");
+        const falseRoute = TestRouter.getRoute("/falsy");
+
+        expect(route).to.be.an.instanceof(Route);
+        expect(falseRoute).to.be.null;
+    })
+
+    it("method 'start' inits routing:", () => {
+        const stub = sinon.stub();
+        TestRouter._onRoute = stub;
+        TestRouter.start();
+        expect(stub.called).to.be.true;
+    });
+
+    it("method 'go' changes history", () => {
+        TestRouter.go("/path");
+        expect(window.history.length).to.eq(2);
+    });
+
+    it("method 'back' triggers history.back", () => {
+        const spy = sinon.spy(window.history, "back");
+        TestRouter.back();
+        expect(spy.calledOnce).to.be.true;
+    });
+    
+      it("method 'forward' triggers history.forward", () => {
+        const forwardSpy = sinon.spy(window.history, "forward");
+        TestRouter.forward();
+        expect(forwardSpy.calledOnce).to.be.true;
+    });
+})
