6c9e1a3e74d53c83687f22915c997d440a8dadaa
callmedim <8431947@gmail.com>
2024-02-25 11:09:21 +0300

    routing adjusted

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-import express from 'express';
+import path, { join } from "path";
+
+import express from "express";
 
 const app = express();
+
 const port = 3000;
 
-app.use(express.static('./build/'));
+app.use(express.static(path.resolve(process.cwd(), "build")));
 
-app.get("/", function (req, res) {
-  res.send("hello sign-in");
-});
-
-app.get("/sign-up", function (req, res) {
-  res.send("hello sign-up");
-});
-
-app.get("/settings", function (req, res) {
-  res.send("hello settings");
-});
-
-
-app.get("/messenger", function (req, res) {
-  res.send("hello messenger");
+app.get("*", (req, res) => {
+  const indexPath = join(path.resolve(process.cwd(), "build"), "index.html");
+  res.sendFile(indexPath);
 });
 
 app.listen(port, () => {
